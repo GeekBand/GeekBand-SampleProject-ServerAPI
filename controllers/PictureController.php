a674b3583edd274f5e5aeac6e1571d100ce3b927
diaosj <groundhacker@gmail.com>
2015-08-25 12:39:09 +0800

    上传图片

---
+++ controllers/PictureController.php
@@ -0,0 +1,170 @@
+<?php
+/**
+ *  ____              _   ____             _
+ *|  _ \  ___  _ __ | |_|  _ \ __ _ _ __ (_) ___
+ *| | | |/ _ \| '_ \| __| |_) / _` | '_ \| |/ __|
+ *| |_| | (_) | | | | |_|  __/ (_| | | | | | (__
+ *|____/ \___/|_| |_|\__|_|   \__,_|_| |_|_|\___|
+ *
+ * Author: diaosj
+ * Date: 15/8/15
+ * Time: 上午9:19
+ */
+
+//namespace app\modules\api\controllers;
+namespace app\controllers;
+
+use Yii;
+use app\models\Picture;
+//use yii\data\ActiveDataProvider;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\db\Query;
+
+class PictureController extends Controller
+{
+
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'view' => ['get'],
+                    'create' => ['post'],
+                    'update' => ['post'],
+                    'delete' => ['delete'],
+                ],
+
+            ]
+        ];
+    }
+
+
+    public function beforeAction($event)
+    {
+        $action = $event->id;
+        if (isset($this->actions[$action])) {
+            $verbs = $this->actions[$action];
+        } elseif (isset($this->actions['*'])) {
+            $verbs = $this->actions['*'];
+        } else {
+            return $event->isValid;
+        }
+        $verb = Yii::$app->getRequest()->getMethod();
+
+        $allowed = array_map('strtoupper', $verbs);
+
+        if (!in_array($verb, $allowed)) {
+
+            $this->setHeader(400);
+            echo json_encode(array('status' => 0, 'error_code' => 400, 'message' => 'Method not allowed'),
+                JSON_PRETTY_PRINT);
+            exit;
+
+        }
+
+        return true;
+    }
+
+    public function actionView()
+    {
+        $nodeId = Yii::$app->request->get('node');
+        if (empty($nodeId)) {
+            $this->setHeader(400);
+            echo json_encode(array('status' => 0, 'error_code' => 400, 'message' => 'Bad request'), JSON_PRETTY_PRINT);
+            exit;
+        }
+
+        $sql = "SELECT * FROM picture WHERE node_id = $nodeId";
+        $pictures = Yii::$app->db->createCommand($sql)->queryAll();
+
+        $this->setHeader(200);
+        echo json_encode(array('status' => 1, 'data' => $pictures), JSON_PRETTY_PRINT);
+
+    }
+
+    public function actionCreate()
+    {
+        $request = Yii::$app->request;
+        $userId = $request->post('user_id');
+        $longtitude = $request->post('longtitude');
+        $latitude = $request->post('latitude');
+        $data = $request->post('data');
+
+        $sql = "select id from node where geom = GeomFromText('Point($longtitude $latitude)')";
+        $nodeId = Yii::$app->db->createCommand($sql)->queryScalar();
+
+        if (empty($nodeId)) {
+            $createSql = "INSERT node (geom) VALUES (GeomFromText('Point($longtitude $latitude)'))";
+            Yii::$app->db->createCommand($createSql)->execute();
+            $nodeId = Yii::$app->db->createCommand($sql)->queryScalar();
+        }
+
+        //picture
+        $img = $data;
+        $save_dir = __DIR__ . '/' . $nodeId . '/';
+        if ( ! file_exists($save_dir)) {
+            mkdir($save_dir);
+        }
+        $fileName = $save_dir . $userId . '_' . time() . '.jpg';
+        $fp2=fopen($fileName,'w');
+        fwrite($fp2,$img);
+        fclose($fp2);
+
+        $model = new Picture();
+        $model->node_id = $nodeId;
+        $model->user_id = $userId;
+        $model->pic_link = $fileName;
+
+
+        if ($model->save()) {
+
+            $this->setHeader(200);
+            echo json_encode(array('status' => 1, 'data' => array_filter($model->attributes)), JSON_PRETTY_PRINT);
+
+        } else {
+            $this->setHeader(400);
+            echo json_encode(array('status' => 0, 'error_code' => 400, 'errors' => $model->errors), JSON_PRETTY_PRINT);
+        }
+
+    }
+
+    protected function findModel($condition)
+    {
+        if (($model = Picture::findOne($condition)) !== null) {
+            return $model;
+        } else {
+
+            $this->setHeader(400);
+            echo json_encode(array('status' => 0, 'error_code' => 400, 'message' => 'Bad request'), JSON_PRETTY_PRINT);
+            exit;
+        }
+    }
+
+    private function setHeader($status)
+    {
+
+        $status_header = 'HTTP/1.1 ' . $status . ' ' . $this->_getStatusCodeMessage($status);
+        $content_type = "application/json; charset=utf-8";
+
+        header($status_header);
+        header('Content-type: ' . $content_type);
+    }
+
+    private function _getStatusCodeMessage($status)
+    {
+        $codes = Array(
+            200 => 'OK',
+            400 => 'Bad Request',
+            401 => 'Unauthorized',
+            402 => 'Payment Required',
+            403 => 'Forbidden',
+            404 => 'Not Found',
+            500 => 'Internal Server Error',
+            501 => 'Not Implemented',
+        );
+        return (isset($codes[$status])) ? $codes[$status] : '';
+    }
+}
---
+++ models/Picture.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "picture".
+ *
+ * @property string $id
+ * @property integer $node_id
+ * @property integer $user_id
+ * @property integer $project_id
+ * @property string $pic_link
+ * @property string $created
+ * @property string $modified
+ */
+class Picture extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'picture';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['node_id', 'user_id', 'project_id'], 'integer'],
+            [['created', 'modified'], 'safe'],
+            [['pic_link'], 'string', 'max' => 64]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'node_id' => 'Node ID',
+            'user_id' => 'User ID',
+            'project_id' => 'Project ID',
+            'pic_link' => 'Pic Link',
+            'created' => 'Created',
+            'modified' => 'Modified',
+        ];
+    }
+}
