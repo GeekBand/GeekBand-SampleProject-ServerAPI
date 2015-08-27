670dab9b9e7cb49c06f778d9428251d281326d08
diaosj <groundhacker@gmail.com>
2015-08-28 05:16:38 +0800

    修改用户名；修改用户头像

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -13,6 +13,7 @@
 
 namespace app\controllers;
 
+use Yii;
 use app\components\CJson;
 use app\components\Util;
 use yii\db\Query;
@@ -21,47 +22,50 @@ use yii\helpers\ArrayHelper;
 use yii\rest\ActiveController;
 use yii\web\User;
 
-class UserController extends ActiveController {
+class UserController extends ActiveController
+{
     public $modelClass = 'app\models\User';
     public $serializer = [
         'class' => 'yii\rest\Serializer',
         'collectionEnvelope' => 'items',
     ];
 
-    public function behaviors() {
+    public function behaviors()
+    {
         $behaviors = parent::behaviors();
         $behaviors['contentNegotiator']['formats'] = [
-                'application/json' => 'json',
+            'application/json' => 'json',
             //todo response not found?
-                //'application/json' => Response::FORMAT_JSON,
+            //'application/json' => Response::FORMAT_JSON,
         ];
 
         return $behaviors;
     }
 
-    public function init() {
+    public function init()
+    {
         parent::init();
-        \Yii::$app->user->enableSession = false;
+        Yii::$app->user->enableSession = false;
     }
 
-    public function actions() {
+    public function actions()
+    {
         $actions = parent::actions();
 
         unset($actions['delete'], $actions['create']);
 
-//        $actions['index'] = '\app\controllers\IndexAction';
-
         return $actions;
     }
 
-    public function actionLogin() {
+    public function actionLogin()
+    {
         $ok = 0;
         //TODO POST
         //$post = $_POST;
         $post = $_REQUEST;
         if (empty($post) or empty($post['username']) or empty($post['password'])) {
             //TODO Unable to locate message source for category 'Global'?
-            //$msg = \Yii::t('Global', 'Wrong Params');
+            //$msg = Yii::t('Global', 'Wrong Params');
             $msg = 'Wrong Params';
             //TODO class 'CJSON' not found?
             //echo CJson::encode(compact('ok', 'msg'));
@@ -70,16 +74,13 @@ class UserController extends ActiveController {
             exit;
         }
 
-        if (empty($post['gbid']))
-        {
+        if (empty($post['gbid'])) {
             $sql = "SELECT * FROM user WHERE name = :username";
             $params = [
                 ':username' => $post['username'],
             ];
-            $record = \Yii::$app->db->createCommand($sql, $params)->queryOne();
-        }
-        else
-        {
+            $record = Yii::$app->db->createCommand($sql, $params)->queryOne();
+        } else {
             $sql = "SELECT * FROM user u
                 JOIN project p ON p.id = u.project_id
                 WHERE u.name = :username AND p.name = :projectName";
@@ -87,7 +88,7 @@ class UserController extends ActiveController {
                 ':username' => $post['username'],
                 ':projectName' => $post['gbid'],
             ];
-            $record = \Yii::$app->db->createCommand($sql, $params)->queryOne();
+            $record = Yii::$app->db->createCommand($sql, $params)->queryOne();
         }
 
         if ($record['password'] == Util::hashPassword($post['password'])) {
@@ -100,7 +101,8 @@ class UserController extends ActiveController {
         exit;
     }
 
-    public function actionRegister() {
+    public function actionRegister()
+    {
         $ok = 0;
         //$post = $_POST;
         $post = $_REQUEST;
@@ -110,16 +112,14 @@ class UserController extends ActiveController {
             exit;
         }
 
-        if ( ! empty($post['gbid']))
-        {
+        if (!empty($post['gbid'])) {
             $sql = "SELECT id FROM project WHERE name = :projectName";
             $params = [
                 ':projectName' => $post['gbid'],
             ];
-            $projectId = \Yii::$app->db->createCommand($sql, $params)->queryScalar();
+            $projectId = Yii::$app->db->createCommand($sql, $params)->queryScalar();
         }
-        if (empty($projectId))
-        {
+        if (empty($projectId)) {
             $projectId = 0;
         }
 
@@ -175,6 +175,163 @@ class UserController extends ActiveController {
         header("Content-Type: application/json");
         echo json_encode(compact('ok', 'msg'));
         exit;
+    }
+
+    public function actionAvatar()
+    {
+        $request = Yii::$app->request;
+        $userName = $request->post('username');
+        $password = $request->post('password');
+        $projectName = $request->post('gbid');
+        $picData = $request->post('data');
+
+        $db = Yii::$app->db;
+        if (empty($projectName)) {
+            $sql = "SELECT id, password FROM user WHERE name = :username";
+            $params = [
+                ':username' => $userName,
+            ];
+        } else {
+            $sql = "SELECT u.id, u.password
+                FROM user u
+                JOIN project p ON p.id = u.project_id
+                WHERE u.name = :username AND p.name = :projectName";
+            $params = [
+                ':username' => $userName,
+                ':projectName' => $projectName,
+            ];
+        }
+        $record = $db->createCommand($sql, $params)->queryOne();
+
+        if (empty($record)) {
+            $this->setHeader(400);
+            echo json_encode(array('status' => 0, 'error_code' => 400, 'message' => 'No such user'),
+                JSON_PRETTY_PRINT);
+            exit;
+        }
+
+        if ($record['password'] != Util::hashPassword($password)) {
+            $this->setHeader(400);
+            echo json_encode(array('status' => 0, 'error_code' => 400, 'message' => 'Invalid password'),
+                JSON_PRETTY_PRINT);
+            exit;
+        }
+
+        $saveDir = __DIR__ . '/../web/images/avatar/';
+        if (!file_exists($saveDir)) {
+            mkdir($saveDir);
+        }
+        $fileName = $saveDir . $record['id'] . '_' . time() . '.jpg';
+        $fp2 = fopen($fileName, 'w');
+        fwrite($fp2, $picData);
+        fclose($fp2);
+
+        $sql = "UPDATE user SET avatar = :path WHERE id = :id";
+        $params = [
+            ':path' => $fileName,
+            ':id' => $record['id'],
+        ];
+        $db->createCommand($sql, $params)->execute();
+
+        $sql = "SELECT id user_id, name user_name, email, avatar FROM user WHERE id = :userId AND avatar = :path";
+        $record = $db->createCommand($sql, $params)->queryOne();
+
+        if (empty($record)) {
+            $this->setHeader(400);
+            echo json_encode(array('status' => 1, 'error_code' => 400, 'message' => 'Database error'),
+                JSON_PRETTY_PRINT);
+            exit;
+        }
+
+        $this->setHeader(200);
+        echo json_encode(array('status' => 1, 'data' => $record, 'message' => 'Update success'), JSON_PRETTY_PRINT);
+        exit;
+    }
+
+    public function actionRename()
+    {
+        $request = Yii::$app->request;
+        $userName = $request->post('username');
+        $password = $request->post('password');
+        $projectName = $request->post('gbid');
+        $newName = $request->post('new_name');
+
+        $db = Yii::$app->db;
+        if (empty($projectName)) {
+            $sql = "SELECT id, password FROM user WHERE name = :username";
+            $params = [
+                ':username' => $userName,
+            ];
+        } else {
+            $sql = "SELECT u.id, u.password
+                FROM user u
+                JOIN project p ON p.id = u.project_id
+                WHERE u.name = :username AND p.name = :projectName";
+            $params = [
+                ':username' => $userName,
+                ':projectName' => $projectName,
+            ];
+        }
+        $record = $db->createCommand($sql, $params)->queryOne();
+
+        if (empty($record)) {
+            $this->setHeader(400);
+            echo json_encode(array('status' => 0, 'error_code' => 400, 'message' => 'No such user'),
+                JSON_PRETTY_PRINT);
+            exit;
+        }
+
+        if ($record['password'] != Util::hashPassword($password)) {
+            $this->setHeader(400);
+            echo json_encode(array('status' => 0, 'error_code' => 400, 'message' => 'Invalid password'),
+                JSON_PRETTY_PRINT);
+            exit;
+        }
+
+        $sql = "UPDATE user SET name = :name WHERE id = :userId";
+        $params = [
+            ':name' => $newName,
+            ':userId' => $record['id'],
+        ];
+        $db->createCommand($sql, $params)->execute();
+
+        $sql = "SELECT id user_id, name user_name, email, avatar FROM user WHERE id = :userId AND name = :name";
+        $record = $db->createCommand($sql, $params)->queryOne();
+
+        if (empty($record)) {
+            $this->setHeader(400);
+            echo json_encode(array('status' => 1, 'error_code' => 400, 'message' => 'Database error'),
+                JSON_PRETTY_PRINT);
+            exit;
+        }
+
+        $this->setHeader(200);
+        echo json_encode(array('status' => 1, 'data' => $record, 'message' => 'Update success'), JSON_PRETTY_PRINT);
+        exit;
+    }
+
+    private function setHeader($status)
+    {
+        $status_header = 'HTTP/1.1 ' . $status . ' ' . $this->_getStatusCodeMessage($status);
+        $content_type = "application/json; charset=utf-8";
+
+        header($status_header);
+        header('Content-type: ' . $content_type);
+    }
+
+    private function _getStatusCodeMessage($status)
+    {
+        $codes = Array(
+            200 => 'OK',
+            400 => 'Bad Request',
+            401 => 'Unauthorized',
+            402 => 'Payment Required',
+            403 => 'Forbidden',
+            404 => 'Not Found',
+            500 => 'Internal Server Error',
+            501 => 'Not Implemented',
+        );
+        return (isset($codes[$status])) ? $codes[$status] : '';
     }
 
 }
