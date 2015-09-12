20debcf5cb45512a2fbe451f78ff97eb482bbcf9
diaosj <groundhacker@gmail.com>
2015-09-12 13:04:39 +0800

    更改字段名

--- controllers/PictureController.php
+++ controllers/PictureController.php
@@ -100,7 +100,7 @@ class PictureController extends Controller
 
         if (empty($nodeId)) {
             $createSql = "INSERT node (geom) VALUES (GeomFromText('Point($longitude $latitude)'))";
-            $address = $request->post('address');
+            $address = $request->post('addr');
             if ($address) {
                 $createSql = "INSERT node(geom, addr) VALUES(GeomFromText('Point($longitude $latitude)'), '$address')";
             }
